b63384c33cb75178f466d52aef9b800d258ac71f
Eric Gagnon <egagnon@pennstatehealth.psu.edu>
2026-09-01 14:00:24 +0000

    Update config.go with 2026-07-01 rvu files link

--- cmsrvu/config.go
+++ cmsrvu/config.go
@@ -80,6 +80,7 @@ var DefaultConfig = Config{
 		{EffectiveDate: parseDate("2025-10-01"), URL: "https://www.cms.gov/files/zip/rvu25d-updated-09/11/2025.zip", FileRegex: ""},
 		{EffectiveDate: parseDate("2026-01-01"), URL: "https://www.cms.gov/files/zip/rvu26a-updated-12-29-2025.zip", FileRegex: ""},
 		{EffectiveDate: parseDate("2026-04-01"), URL: "https://www.cms.gov/files/zip/rvu26b-updated-03-10-2026.zip", FileRegex: ""},
+		{EffectiveDate: parseDate("2026-07-01"), URL: "https://www.cms.gov/files/zip/rvu26c-updated-06-30-2026.zip", FileRegex: ""},
 	},
 }
 
